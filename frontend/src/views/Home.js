abb976c5ef07de785af2ccf2cd785e178d379c4d
Sebastian <sebastian.danielsson@proton.me>
2023-03-20 13:50:21 +0100

    Break down home into separate components

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,42 +1,8 @@
-import {Alert, Box, Grid, Snackbar, Typography} from '@mui/material';
-import {useState} from 'react';
-import {useLocation} from 'react-router-dom';
+/* import ItemComponent from '../components/ItemComponent'; */
+import ItemList from '../components/ItemList';
 
 function Home() {
-  const location = useLocation();
-
-  const message = location?.state?.message;
-  const [alertOpen, setAlertOpen] = useState(location.state != null);
-  return (
-    <>
-      <Snackbar
-        open={alertOpen}
-        anchorOrigin={{vertical: 'top', horizontal: 'center'}}
-        autoHideDuration={2000}
-        onClose={() => {
-          setAlertOpen(false);
-          location.state = null;
-        }}>
-        <Alert severity="success" sx={{width: '100%'}}>
-          {message}
-        </Alert>
-      </Snackbar>
-      <Grid container columnSpacing={8} p={2} className="Home">
-        <Grid item xs={12} md={8}>
-          <Typography variant="h2">Alla inlägg</Typography>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Box marginBottom="4rem">
-            <Typography variant="h2">Användare</Typography>
-          </Box>
-          <Box>
-            <Typography variant="h2">Taggar</Typography>
-          </Box>
-        </Grid>
-      </Grid>
-    </>
-  );
+  <ItemList></ItemList>;
 }
 
 export default Home;
