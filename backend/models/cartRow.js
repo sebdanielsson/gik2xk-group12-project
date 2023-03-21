63924f1f13995cacf33a0ea19d2a8e77897f1c01
h21veren <h21veren@du.se>
2023-03-21 23:13:56 +0100

    Backend routes fixade
    Alla routes bör fungera nu

--- backend/models/cartRow.js
+++ backend/models/cartRow.js
@@ -1,3 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('cartRow', {}, {underscored: true});
+  return sequelize.define(
+    'cartRow',
+    {
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {underscored: true},
+  );
 };
