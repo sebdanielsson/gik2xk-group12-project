b0bd7c27eb057af2a7603419fcde09ef3acec698
Sebastian <sebastian.danielsson@proton.me>
2023-03-22 21:42:37 +0100

    Design tweaks to ProductCard

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -5,28 +5,40 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import {CardActionArea} from '@mui/material';
 import RatingItem from './RatingItem';
-import BasicButton from './BasicButton';
+import {CardActions} from '@mui/material';
+import {Button} from '@mui/material';
+import {Link} from 'react-router-dom';
 
 function ProductCard(props) {
   const {product} = props;
   return (
     <Card sx={{maxWidth: 345}}>
-      <RatingItem />
-      <CardActionArea>
-        <CardMedia component="img" height="140" image={product.imageUrl} alt={product.title} />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {product.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {product.description}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {product.price}
-          </Typography>
-          <BasicButton></BasicButton>
-        </CardContent>
-      </CardActionArea>
+      <Link to={`/products/${product.id}`} style={{textDecoration: 'inherit', color: 'inherit'}}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={
+              product.imageUrl ||
+              'https://image.spreadshirtmedia.net/image-server/v1/compositions/T812A2PA5886PT17X48Y77D155266323W24996H9381/views/1,width=550,height=550,appearanceId=2,backgroundColor=000000,noPt=true/meh-ascii-art-internet-emoticon-emoji-mens-premium-t-shirt.jpg'
+            }
+            alt={product.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" style={{marginBottom: 10}}>
+              {product.description}
+            </Typography>
+            <RatingItem />
+          </CardContent>
+        </CardActionArea>
+      </Link>
+      <CardActions style={{justifyContent: 'space-between'}}>
+        <Typography variant="body1">{product.price} kr</Typography>
+        <Button variant="text">Add to cart</Button>
+      </CardActions>
     </Card>
   );
 }
