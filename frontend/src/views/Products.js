b02bcdb58f4fea8aacbad1448e77b74ba0d5236c
Sebastian <sebastian.danielsson@proton.me>
2023-03-23 17:11:01 +0100

    Add "addProduct" floating action button to product page

--- frontend/src/views/Products.js
+++ frontend/src/views/Products.js
@@ -1,10 +1,19 @@
 import {Container} from '@mui/material';
 import ProductList from '../components/ProductList';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 function Products() {
   return (
     <Container maxWidth="md">
       <ProductList pathname="products" />
+      <Fab
+        color="primary"
+        aria-label="add"
+        href="/products/new"
+        sx={{position: 'fixed', bottom: '50px', right: '50px'}}>
+        <AddIcon />
+      </Fab>
     </Container>
   );
 }
